5585d310e88e9c7662dd0fab57065d825ee86d8a
Kuleen Sasse <ksasse@uab.edu>
2022-01-18 12:32:09 -0600

    CREATED: NEW UTIL FUNCTION TO CLASSIFY PACKET CREATED NEW MAIN FILE TO RUN DATA ANALYSIS REMOVED READMCPR.IPYNB AS IT WAS DRAFTING AREA

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+import struct
+import argparse
+from utils.functions import classify_packet
+from os.path import exists
+
+
+def main(args):
+    #read the protocol for 1.8
+    #Protocol 47 
+    #JSON from https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/1.8/protocol.json
+    protocol = json.load(open('./data/protocol.json', 'r'))
+
+    #creates a mapping between id numbers and names of packets
+    packet_json = protocol['play']['toClient']['types']
+    #gets the mappings from the protocol
+    packet_types = packet_json['packet'][1][0]['type'][1]['mappings']
+
+    #adds packet_ in front of it to work with the keys of packet_json
+    packet_types_values = ["packet_"+x for x in list(packet_types.values())]
+
+    #converts the hex string ids to numbers
+    packet_types_keys = [int(x, 16) for x in list(packet_types.keys())]
+
+    #creates a dictionary of them
+    packet_types = dict(zip(packet_types_keys, packet_types_values))
+
+    packets = []
+    if not exists(args.file):
+        raise ValueError('File does not exist')
+    elif args.files.split('.')[-1]!="tmpcr":
+        raise ValueError('File is in wrong format')
+    with open(args.file, "rb") as f:
+        while timestamp := f.read(4):
+            timestamp = struct.unpack('>i', timestamp)[0]
+            length = struct.unpack('>i', f.read(4))[0]
+            byte_array = f.read(length)
+            id = byte_array[0]
+            byte_array = byte_array[1:]
+            length-=1
+            packets.append(classify_packet(timestamp, length, byte_array, id))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract position data from 1.8 mcpr files')
+    parser.add_argument('-f', '--file', help='file path to tmcpr file', required=True)
+    args = parser.parse_args()
+    main(args)
--- utils/functions.py
+++ utils/functions.py
@@ -1,3 +1,5 @@
+import Packets
+
 def read_var_int(b:bytes) -> tuple[int, bytes]:
     value = 0
     length = 0
@@ -8,3 +10,39 @@ def read_var_int(b:bytes) -> tuple[int, bytes]:
         if ((value & 0x80) != 0x80):
             break
     return value, b[length:]
+
+def classify_packet(timestamp: int, length: int, byte_array:bytes, id:int) -> Packets.Packet:
+    """0x0C Spawn Player 12
+    0x12 Entity Velocity 18
+    0x14 Entity 20
+    0x15 Entity Relative Move 21
+    0x16 Entity Look 22
+    0x17 Entity Look And Relative Move 23
+    0x18 Entity Teleport 24
+    0x19 Entity Head Look 25
+    0x1A Entity Status 26
+    0x1C Entity Metadata 28"""
+    if id==12:
+        packet = Packets.SpawnPlayerPacket(timestamp, length, byte_array, id)
+    elif id==18:
+        packet = Packets.EntityVelocityPacket(timestamp, length, byte_array, id)
+    elif id==20:
+        packet = Packets.EntityPacket(timestamp, length, byte_array, id)
+    elif id==21:
+        packet = Packets.EntityRelativeMovePacket(timestamp, length, byte_array, id)
+    elif id==22:
+        packet = Packets.EntityLookPacket(timestamp, length, byte_array, id)
+    elif id==23:
+        packet = Packets.EntityLookAndRelativeMovePacket(timestamp, length, byte_array, id)
+    elif id==24:
+        packet = Packets.EntityTeleportPacket(timestamp, length, byte_array, id)
+    elif id==25:
+        packet = Packets.EntityHeadLookPacket(timestamp, length, byte_array, id)
+    elif id==26:
+        packet = Packets.EntityStatusPacket(timestamp, length, byte_array, id)
+    elif id==28:
+        packet = Packets.EntityMetadataPacket(timestamp, length, byte_array, id)
+    else:
+        packet = None
+    return packet
+
