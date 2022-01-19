467b63b20c57ebe7869d6a17103253bec10622c0
Katsuki Chan <katsukidotxyz@gmail.com>
2022-01-19 11:50:56 -0500

    feat: initial reading custom metadata

--- Packets/EntityMetadataPacket.py
+++ Packets/EntityMetadataPacket.py
@@ -1,8 +1,15 @@
 from Packets.Packet import Packet
+import io
+import struct
+from utils.decode import read_var_int, read_metadata
+
 
 class EntityMetadataPacket(Packet):
-    def __init__(self, timestamp: int, length: int, byte_array, id:int):
+    def __init__(self, timestamp: int, length: int, byte_array, id: int):
         super().__init__(timestamp, length, byte_array, id)
+        self.entity_id = None
+        self.metadata = None
 
     def decode(self):
-        pass
+        eid, b = read_var_int(self.byte_array)
+        self.entity_id = eid
--- utils/decode.py
+++ utils/decode.py
@@ -1,12 +1,27 @@
 import io
 import struct
 
+
 def read_var_int(buff: bytes) -> "tuple[int, bytes]":
     buff = io.BytesIO(buff)
     total = 0
     shift = 0
     val = 0x80
     while val & 0x80:
+        val = struct.unpack('>B', buff.read(1))[0]
+        total |= ((val & 0x7F) << shift)
+        shift += 7
+    if total & (1 << 31):
+        total = total - (1 << 32)
+    return total, buff.read()
+
+
+def read_metadata(buff: bytes) -> "tuple[int, bytes]":
+    buff = io.BytesIO(buff)
+    total = 0
+    shift = 0
+    val = 0xff
+    while val & 0xff:
         val = struct.unpack('>B', buff.read(1))[0]
         total |= ((val & 0x7F) << shift)
         shift += 7
